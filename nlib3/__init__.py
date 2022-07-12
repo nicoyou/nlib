44e82c459a4864c9d668ca2bc91e1e90975b4671
羽咲蒼 <50352295+nicoyou@users.noreply.github.com>
2022-07-12 16:21:18 +0900

    add: 個人用ライブラリである nlib の ver.1.14.0 を追加

---
+++ nlib3/__init__.py
@@ -0,0 +1,8 @@
+from .nlib import *
+
+__copyright__ = "Copyright (C) 2022 Nicoyou"
+__version__ = "1.14.0"
+__license__ = "MIT"
+__author__ = "nicoyou"
+__author_email__ = "nicoyou.info@gmail.com"
+__url__ = "https://github.com/nicoyou/nlib"
