d484283e99d852d8983503f8a69c0dece6087850
羽咲蒼 <50352295+nicoyou@users.noreply.github.com>
2023-12-21 14:06:37 +0900

    v1.19.0

--- setup.py
+++ setup.py
@@ -25,18 +25,18 @@ assert author_email
 assert url
 
 setuptools.setup(
-    name=package_name,                                              # パッケージ名 ( プロジェクト名 )
-    packages=[package_name.replace("-", "_")],                      # パッケージ内 ( プロジェクト内 ) のパッケージ名をリスト形式で指定 ( ここを指定しないとパッケージが含まれずに、テキストのみのパッケージになってしまう )
-    version=version,                                                # バージョン
-    license=license,                                                # ライセンス
-    install_requires=[],                                            # pip installする際に同時にインストールされるパッケージ名をリスト形式で指定
-    author=author,                                                  # パッケージ作者の名前
-    author_email=author_email,                                      # パッケージ作者の連絡先メールアドレス
-    url=url,                                                        # パッケージに関連するサイトのURL ( GitHubなど )
-    description="You can use a slightly useful function.",          # パッケージの簡単な説明
-    long_description=long_description,                              # PyPIに"Project description"として表示されるパッケージの説明文
-    long_description_content_type="text/markdown",                  # long_descriptionの形式を"text/plain", "text/x-rst", "text/markdown"のいずれかから指定
-    keywords=f"{package_name} nlib library nicoyou_lib nicoyou",    # PyPIでの検索用キーワードをスペース区切りで指定
+    name=package_name,                                                      # パッケージ名 ( プロジェクト名 )
+    packages=[package_name.replace("-", "_")],                              # パッケージ内 ( プロジェクト内 ) のパッケージ名をリスト形式で指定 ( ここを指定しないとパッケージが含まれずに、テキストのみのパッケージになってしまう )
+    version=version,                                                        # バージョン
+    license=license,                                                        # ライセンス
+    install_requires=[],                                                    # pip install する際に同時にインストールされるパッケージ名をリスト形式で指定
+    author=author,                                                          # パッケージ作者の名前
+    author_email=author_email,                                              # パッケージ作者の連絡先メールアドレス
+    url=url,                                                                # パッケージに関連するサイトの URL ( GitHub など )
+    description="You can use a slightly useful function.",                  # パッケージの簡単な説明
+    long_description=long_description,                                      # PyPI に "Project description" として表示されるパッケージの説明文
+    long_description_content_type="text/markdown",                          # long_description の形式を "text/plain", "text/x-rst", "text/markdown" のいずれかから指定
+    keywords=f"{package_name} nlib library utility nicoyou_lib nicoyou",    # PyPI での検索用キーワードをスペース区切りで指定
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
@@ -44,5 +44,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
-    ],                                                              # パッケージ ( プロジェクト ) の分類 ( https://pypi.org/classifiers/ )
+    ],                                                                      # パッケージ ( プロジェクト ) の分類 ( https://pypi.org/classifiers/ )
 )
